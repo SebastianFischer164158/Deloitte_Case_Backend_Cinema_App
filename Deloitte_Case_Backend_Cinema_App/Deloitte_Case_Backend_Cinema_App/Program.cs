6bfd103113bc98c64e45609d39ea8fa13c8831b3
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-18 10:01:22 +0100

    Program.cs: Implemented menu handler logic and various choices

--- Deloitte_Case_Backend_Cinema_App/Deloitte_Case_Backend_Cinema_App/Program.cs
+++ Deloitte_Case_Backend_Cinema_App/Deloitte_Case_Backend_Cinema_App/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Deloitte_Case_Backend_Cinema_App
 {
@@ -11,6 +11,59 @@ namespace Deloitte_Case_Backend_Cinema_App
             Cinema cinema = new Cinema(numberOfRows, numberOfSeatsPerRow);
             
             Console.WriteLine($"You entered:\nRows: {numberOfRows} \nSeats per row: {numberOfSeatsPerRow}\n");
+            MenuHandler(cinema);
+        }
+
+        private static void MenuHandler(Cinema cinema)
+        {
+            bool userExit = false;
+            while (!userExit)
+            {
+                Console.WriteLine("------------------ MENU ------------------");
+                Console.WriteLine("Press 1 to buy a Ticket");
+                Console.WriteLine("Press 2 to show seats and their status");
+                Console.WriteLine("Press 3 to output metrics for the cinema room");
+                Console.WriteLine("Press 4 to exit and close the app");
+                Console.WriteLine("------------------------------------------");
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.WriteLine("------------ You pressed 1 --------------");
+                        if (cinema.NumberOfPurchasedTickets == cinema.Capacity)
+                        {
+                            Console.WriteLine("The cinema is fully booked!");
+                            break;
+                        }
+                        int userSeat = GetSeatInput(cinema);
+                        cinema.BookSeat(userSeat);
+                        break;
+                    case "2":
+                        Console.WriteLine("------------ You pressed 2 --------------");
+                        cinema.OutputSeatsAndStatus();
+                        break;
+                    case "3":
+                        Console.WriteLine("------------ You pressed 3 --------------");
+                        decimal percentageOccupied = cinema.CalculatePercentageOccupied();
+                        Console.WriteLine($"Number of purchased tickets: {cinema.NumberOfPurchasedTickets}\n" +
+                                          $"Percentage currently occupied: {percentageOccupied:N3}%\n" +
+                                          $"Current Income (Sum of Reserved Tickets): {cinema.CalculateCurrentIncome()}\n" + 
+                                          $"Potential total income: {cinema.PotentialTotalIncome()}\n");
+                        break;
+                    case "4":
+                        Console.WriteLine("------------ You pressed 4 --------------");
+                        Console.WriteLine("------------    EXITING    --------------");
+                        userExit = true;
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Wrong option entered - Please pick one between 1 <-> 4");
+                        break;
+                }
+            }
+        }
+
         private static int GetSeatInput(Cinema cinema)
         {
             int seatNumber = 0;
