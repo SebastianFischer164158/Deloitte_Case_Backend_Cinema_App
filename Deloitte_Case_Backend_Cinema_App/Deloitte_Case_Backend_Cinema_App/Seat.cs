646ed564b3c9dac134397d78fa96a36e6a1c1c58
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-18 09:42:10 +0100

    Seat.cs: created class with SeatNumber, SeatStatus
    struct could also have been used

--- Deloitte_Case_Backend_Cinema_App/Deloitte_Case_Backend_Cinema_App/Seat.cs
+++ Deloitte_Case_Backend_Cinema_App/Deloitte_Case_Backend_Cinema_App/Seat.cs
@@ -2,6 +2,9 @@
 {
     public class Seat
     {
-        
+        //probably should've just used a struct
+        public int SeatNumber { get; set; }
+        //Let's assume that initially everything is available, e.g. all seats are 'A'
+        public char SeatStatus { get; set; } = 'A';
     }
 }
