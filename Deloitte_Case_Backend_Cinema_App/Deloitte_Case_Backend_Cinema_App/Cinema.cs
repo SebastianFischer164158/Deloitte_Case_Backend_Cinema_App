4e1bb47761bd500e2f3f8a424be87cab5c95184b
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-18 09:50:05 +0100

    Cinema.cs added Cinema class constructor & PopulateCinemaRoom

--- Deloitte_Case_Backend_Cinema_App/Deloitte_Case_Backend_Cinema_App/Cinema.cs
+++ Deloitte_Case_Backend_Cinema_App/Deloitte_Case_Backend_Cinema_App/Cinema.cs
@@ -1,7 +1,34 @@
-﻿namespace Deloitte_Case_Backend_Cinema_App
+using System;
+
+namespace Deloitte_Case_Backend_Cinema_App
 {
     public class Cinema
     {
+        private readonly int _rows; 
+        private readonly int _seats; //columns
         
+        private static Seat[,] _seatArray;
+        public Cinema(int numberOfRows, int numberOfSeatsPerRow)
+        {
+            _rows = numberOfRows;
+            _seats = numberOfSeatsPerRow;
+            Capacity = _rows * _seats;
+            _seatArray = new Seat[numberOfRows, numberOfSeatsPerRow];
+            PopulateCinemaRoom(numberOfRows, numberOfSeatsPerRow);
+        }
+
+        private static void PopulateCinemaRoom(int numberOfRows, int numberOfSeatsPerRow)
+        {
+            int counter = 0;
+            for (int iRow = 0; iRow < numberOfRows; iRow++)
+            {
+                for (int jSeat = 0; jSeat < numberOfSeatsPerRow; jSeat++)
+                {
+                    _seatArray[iRow, jSeat] = new Seat {SeatNumber = counter};
+                    counter += 1;
+                }
+            }
+        }
+        }
     }
 }
